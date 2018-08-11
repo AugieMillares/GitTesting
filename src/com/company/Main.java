852b43fda5e1ef6dd8e771c3a0618ebd9127ee94
augiemillares <augie@acadaca.com>
2018-08-11 09:07:40 -0400

    test for conflict in repo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	       // write your code here
         System.out.println("Hello World");
-        // comment from repo
+        //This is my intellij Changes
+        System.out.printf("Any word");
     }
 }
